90fa4befb1958096f38d401ae9f89f1a94297635
missarushi-coder <arushi.2023.1767@mirandahouse.ac.in>
2024-09-13 01:24:51 +0530

    Update task2.cpp
    created random number generator

--- task2.cpp
+++ task2.cpp
@@ -1,1 +1,11 @@
-
+#include <cstdlib>
+#include <iostream>
+#include <time.h>
+using namespace std;
+int main(){
+    int n=100000;
+//Generating random numbers on each go.
+    srand(time(0));
+    cout<< rand()% n ;
+    return 0;
+}
