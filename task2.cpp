e5b74723670687b7160fce3699599bb6d386c831
missarushi-coder <arushi.2023.1767@mirandahouse.ac.in>
2024-09-13 03:36:34 +0530

    Update task2.cpp
    All the conditions checked.
    The mini-version of game is ready.

--- task2.cpp
+++ task2.cpp
@@ -1,40 +1,47 @@
 #include <cstdlib>
 #include <iostream>
 #include <time.h>
+#include <math.h>
 using namespace std;
 int main(){
-    cout<<"Let's play number-guess\n\n";
+    cout<<"NUMBER-GUESS\n\n";
+    cout<<"Rules:\n 1. You will have only 7 trials.\n 2. you will be alerted with where yo exist. like "<<"Too low means below 25%; low means between 25-50% . close means 50-75%,and too close means 75-99%\n";
+    cout<<"Hint: The no. of stars is the number of digit contained in the guess-number\n\n";
+    cout<<"Let's play!\n\n";
     //Generating random numbers on each go.
     int n;
-    cout<<"Enter the no. of digits for the Guess number";
+    cout<<"Enter the last limit of the number ";
     cin>>n;
     srand(time(0));
     int num = rand()% n ;
+    //Creating the symbol as hidden right digits
+    int l=int(log10(num)+1);
+    for (int i=1; i<=l; i++ ){
+        cout<<" * ";
+    }
+    cout<<num<<endl;   //to get the right number
     
     //player interaction
-    cout<<"*****"<<endl;
-    cout<<num<<endl;
     cout<<"Guess the number!"<<endl;
-    int guess;
-    cin>>guess;
+    int guess=-1;
     int i=1;
-    while (guess != num & i<5){
-         cout<<"Guess Again";
-        cout<<"  ";
+    while (guess != num & i<7){
         cin>>guess;
         if (guess==num){
             cout<<"Wuhoo Right Guess!\n\n";
             break;
-            }else if (guess < (num/4) & guess >(num/10)){
+            }else if (guess < num*0.25){
                  cout<<"Guess is too low!\n";
-            }else if(guess < (num/2)){
+            }else if(guess < num*0.5){
                  cout<<"Guess is low!\n";
-            }else if(guess > (num/2) & guess<(num)){
+            }else if(guess < num*0.75 ){
                  cout<<"Guess is close!\n";
-            }else if(guess > (num*(3/4)) & guess<(num)){
-                 cout<<"Guess is high!\n";
+            }else if(guess > num*0.75 & guess < num){
+                 cout<<"Guess is close!\n";
             }else
-                 cout<<"Guess is too high!\n";
+                 cout<<"Guess is too far!\n";
+            cout<<"Guess Again";
+            cout<<"  ";
         i++;
     }
       cout<<"Try Again with a new number";  
